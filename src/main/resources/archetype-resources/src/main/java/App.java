9a4f2261437dda0c94b07f338773c7db65dd8666
SwingGuy1024 <SwingGuy1024@yahoo.com>
2021-12-02 16:40:01 -0800

    Initial commit

---
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,12 @@
+/**
+ * <p>Created by IntelliJ IDEA.
+ * <p>Date: 12/2/21
+ * <p>Time: 3:59 PM
+ *
+ * @author Miguel Mu\u00f1oz
+ */
+public class App {
+  public static void main(String[] args) {
+    
+  }
+}
---
+++ src/main/resources/archetype-resources/src/test/java/AppTest.java
@@ -0,0 +1,9 @@
+/**
+ * <p>Created by IntelliJ IDEA.
+ * <p>Date: 12/2/21
+ * <p>Time: 3:59 PM
+ *
+ * @author Miguel Mu\u00f1oz
+ */
+public class AppTest {
+}
